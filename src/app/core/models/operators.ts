9370154cae16d1297f3994df807f554b45fb14d5
Grzegorz Ciesielski <tao24.k@gmail.com>
2021-04-27 11:35:21 +0200

    perf: making admin-service cache data

--- src/app/core/models/operators.ts
+++ src/app/core/models/operators.ts
@@ -1,10 +1,31 @@
-import { combineLatest, forkJoin, Observable } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 export function all(...observables: Observable<any>[]): Observable<boolean> {
-  return forkJoin(observables).pipe(map((results) => results.reduce((a, b) => !!a && !!b)));
+  return combineLatest(observables).pipe(map((results) => results.reduce((a, b) => !!a && !!b)));
 }
 
 export function any(...observables: Observable<any>[]): Observable<boolean> {
   return combineLatest(observables).pipe(map((results) => results.reduce((a, b) => !!a || !!b)));
+}
+
+export function firstNonNull() {
+  return function <T>(source: Observable<T>): Observable<boolean> {
+    return new Observable((subscriber) => {
+      return source.subscribe({
+        next(value) {
+          if (!!value) {
+            subscriber.next(true);
+            subscriber.complete();
+          }
+        },
+        complete() {
+          subscriber.complete();
+        },
+        error(error) {
+          subscriber.error(error);
+        },
+      });
+    });
+  };
 }
